20e674f15200d03057750f46ab168f6653c3e69b
Tommy Choi <40275760+talryu@users.noreply.github.com>
2018-06-18 14:23:33 -0400

    Add files via upload

---
+++ Erica Faye Barriatos - Colour Puzzle Game/Erica Faye Barriatos - Colour Puzzle Game/Form1.Designer.vb
@@ -0,0 +1,342 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnNew = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn14 = New System.Windows.Forms.Button()
+        Me.btn13 = New System.Windows.Forms.Button()
+        Me.btn12 = New System.Windows.Forms.Button()
+        Me.btn11 = New System.Windows.Forms.Button()
+        Me.btn16 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn10 = New System.Windows.Forms.Button()
+        Me.btn15 = New System.Windows.Forms.Button()
+        Me.lblTitletime = New System.Windows.Forms.Label()
+        Me.lblTime = New System.Windows.Forms.Label()
+        Me.lblScoreTitle = New System.Windows.Forms.Label()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.btnInstructions = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnNew
+        '
+        Me.btnNew.BackgroundImage = Global.Erica_Faye_Barriatos___Bejeweled.My.Resources.Resources.Bejeweled_Blitz___x7
+        Me.btnNew.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnNew.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnNew.Image = Global.Erica_Faye_Barriatos___Bejeweled.My.Resources.Resources.Bejeweled_Blitz___x7
+        Me.btnNew.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.btnNew.Location = New System.Drawing.Point(15, 12)
+        Me.btnNew.Name = "btnNew"
+        Me.btnNew.Size = New System.Drawing.Size(159, 40)
+        Me.btnNew.TabIndex = 0
+        Me.btnNew.Text = "New Game"
+        Me.btnNew.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.Color.Black
+        Me.btn1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn1.Location = New System.Drawing.Point(15, 61)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(77, 62)
+        Me.btn1.TabIndex = 1
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.Color.Black
+        Me.btn2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn2.Location = New System.Drawing.Point(98, 61)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(77, 62)
+        Me.btn2.TabIndex = 2
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.Color.Black
+        Me.btn3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn3.Location = New System.Drawing.Point(181, 61)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(77, 62)
+        Me.btn3.TabIndex = 3
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.Color.Black
+        Me.btn4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn4.Location = New System.Drawing.Point(264, 61)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(77, 62)
+        Me.btn4.TabIndex = 4
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.Color.Black
+        Me.btn9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn9.Location = New System.Drawing.Point(14, 197)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(77, 62)
+        Me.btn9.TabIndex = 9
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.Color.Black
+        Me.btn8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn8.Location = New System.Drawing.Point(264, 129)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(77, 62)
+        Me.btn8.TabIndex = 8
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.Color.Black
+        Me.btn7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn7.Location = New System.Drawing.Point(181, 129)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(77, 62)
+        Me.btn7.TabIndex = 7
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.Color.Black
+        Me.btn6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn6.Location = New System.Drawing.Point(98, 129)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(77, 62)
+        Me.btn6.TabIndex = 6
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn14
+        '
+        Me.btn14.BackColor = System.Drawing.Color.Black
+        Me.btn14.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn14.Location = New System.Drawing.Point(98, 265)
+        Me.btn14.Name = "btn14"
+        Me.btn14.Size = New System.Drawing.Size(77, 62)
+        Me.btn14.TabIndex = 14
+        Me.btn14.UseVisualStyleBackColor = False
+        '
+        'btn13
+        '
+        Me.btn13.BackColor = System.Drawing.Color.Black
+        Me.btn13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn13.Location = New System.Drawing.Point(15, 265)
+        Me.btn13.Name = "btn13"
+        Me.btn13.Size = New System.Drawing.Size(77, 62)
+        Me.btn13.TabIndex = 13
+        Me.btn13.UseVisualStyleBackColor = False
+        '
+        'btn12
+        '
+        Me.btn12.BackColor = System.Drawing.Color.Black
+        Me.btn12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn12.Location = New System.Drawing.Point(264, 197)
+        Me.btn12.Name = "btn12"
+        Me.btn12.Size = New System.Drawing.Size(77, 62)
+        Me.btn12.TabIndex = 12
+        Me.btn12.UseVisualStyleBackColor = False
+        '
+        'btn11
+        '
+        Me.btn11.BackColor = System.Drawing.Color.Black
+        Me.btn11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn11.Location = New System.Drawing.Point(181, 197)
+        Me.btn11.Name = "btn11"
+        Me.btn11.Size = New System.Drawing.Size(77, 62)
+        Me.btn11.TabIndex = 11
+        Me.btn11.UseVisualStyleBackColor = False
+        '
+        'btn16
+        '
+        Me.btn16.BackColor = System.Drawing.Color.Black
+        Me.btn16.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn16.Location = New System.Drawing.Point(264, 265)
+        Me.btn16.Name = "btn16"
+        Me.btn16.Size = New System.Drawing.Size(77, 62)
+        Me.btn16.TabIndex = 16
+        Me.btn16.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.Color.Black
+        Me.btn5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn5.Location = New System.Drawing.Point(15, 129)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(77, 62)
+        Me.btn5.TabIndex = 5
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn10
+        '
+        Me.btn10.BackColor = System.Drawing.Color.Black
+        Me.btn10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn10.Location = New System.Drawing.Point(97, 197)
+        Me.btn10.Name = "btn10"
+        Me.btn10.Size = New System.Drawing.Size(77, 62)
+        Me.btn10.TabIndex = 10
+        Me.btn10.UseVisualStyleBackColor = False
+        '
+        'btn15
+        '
+        Me.btn15.BackColor = System.Drawing.Color.Black
+        Me.btn15.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn15.Location = New System.Drawing.Point(181, 265)
+        Me.btn15.Name = "btn15"
+        Me.btn15.Size = New System.Drawing.Size(77, 62)
+        Me.btn15.TabIndex = 15
+        Me.btn15.UseVisualStyleBackColor = False
+        '
+        'lblTitletime
+        '
+        Me.lblTitletime.AutoSize = True
+        Me.lblTitletime.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lblTitletime.Image = Global.Erica_Faye_Barriatos___Bejeweled.My.Resources.Resources._1
+        Me.lblTitletime.Location = New System.Drawing.Point(12, 358)
+        Me.lblTitletime.Name = "lblTitletime"
+        Me.lblTitletime.Size = New System.Drawing.Size(61, 23)
+        Me.lblTitletime.TabIndex = 17
+        Me.lblTitletime.Text = "Time:"
+        '
+        'lblTime
+        '
+        Me.lblTime.Image = Global.Erica_Faye_Barriatos___Bejeweled.My.Resources.Resources._2
+        Me.lblTime.Location = New System.Drawing.Point(77, 358)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(98, 23)
+        Me.lblTime.TabIndex = 18
+        '
+        'lblScoreTitle
+        '
+        Me.lblScoreTitle.AutoSize = True
+        Me.lblScoreTitle.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lblScoreTitle.Image = Global.Erica_Faye_Barriatos___Bejeweled.My.Resources.Resources._31
+        Me.lblScoreTitle.Location = New System.Drawing.Point(201, 358)
+        Me.lblScoreTitle.Name = "lblScoreTitle"
+        Me.lblScoreTitle.Size = New System.Drawing.Size(63, 23)
+        Me.lblScoreTitle.TabIndex = 19
+        Me.lblScoreTitle.Text = "Score:"
+        '
+        'lblScore
+        '
+        Me.lblScore.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lblScore.Image = Global.Erica_Faye_Barriatos___Bejeweled.My.Resources.Resources._4
+        Me.lblScore.Location = New System.Drawing.Point(270, 358)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(63, 23)
+        Me.lblScore.TabIndex = 20
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'btnInstructions
+        '
+        Me.btnInstructions.BackColor = System.Drawing.Color.Transparent
+        Me.btnInstructions.BackgroundImage = Global.Erica_Faye_Barriatos___Bejeweled.My.Resources.Resources._2
+        Me.btnInstructions.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnInstructions.Location = New System.Drawing.Point(216, 12)
+        Me.btnInstructions.Name = "btnInstructions"
+        Me.btnInstructions.Size = New System.Drawing.Size(135, 39)
+        Me.btnInstructions.TabIndex = 21
+        Me.btnInstructions.Text = "Instruction"
+        Me.btnInstructions.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 23.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightGray
+        Me.BackgroundImage = Global.Erica_Faye_Barriatos___Bejeweled.My.Resources.Resources.Bejeweled_Blitz___x7
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(363, 403)
+        Me.Controls.Add(Me.btnInstructions)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.lblScoreTitle)
+        Me.Controls.Add(Me.lblTime)
+        Me.Controls.Add(Me.lblTitletime)
+        Me.Controls.Add(Me.btn16)
+        Me.Controls.Add(Me.btn15)
+        Me.Controls.Add(Me.btn14)
+        Me.Controls.Add(Me.btn13)
+        Me.Controls.Add(Me.btn12)
+        Me.Controls.Add(Me.btn11)
+        Me.Controls.Add(Me.btn10)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btnNew)
+        Me.Font = New System.Drawing.Font("Century Schoolbook", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.Name = "Form1"
+        Me.Text = "Colour Puzzle Game "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnNew As System.Windows.Forms.Button
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents btn3 As System.Windows.Forms.Button
+    Friend WithEvents btn4 As System.Windows.Forms.Button
+    Friend WithEvents btn9 As System.Windows.Forms.Button
+    Friend WithEvents btn8 As System.Windows.Forms.Button
+    Friend WithEvents btn7 As System.Windows.Forms.Button
+    Friend WithEvents btn6 As System.Windows.Forms.Button
+    Friend WithEvents btn14 As System.Windows.Forms.Button
+    Friend WithEvents btn13 As System.Windows.Forms.Button
+    Friend WithEvents btn12 As System.Windows.Forms.Button
+    Friend WithEvents btn11 As System.Windows.Forms.Button
+    Friend WithEvents btn16 As System.Windows.Forms.Button
+    Friend WithEvents btn5 As System.Windows.Forms.Button
+    Friend WithEvents btn10 As System.Windows.Forms.Button
+    Friend WithEvents btn15 As System.Windows.Forms.Button
+    Friend WithEvents lblTitletime As System.Windows.Forms.Label
+    Friend WithEvents lblTime As System.Windows.Forms.Label
+    Friend WithEvents lblScoreTitle As System.Windows.Forms.Label
+    Friend WithEvents lblScore As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents btnInstructions As System.Windows.Forms.Button
+
+End Class
---
+++ Erica Faye Barriatos - Colour Puzzle Game/Erica Faye Barriatos - Colour Puzzle Game/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Erica Faye Barriatos - Bejeweled")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Erica Faye Barriatos - Bejeweled")> 
+<Assembly: AssemblyCopyright("Copyright ©  2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2086b25f-5485-49f6-b1da-e6e11fdc7814")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
